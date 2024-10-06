f6f90e3f2d2a225dccd746b995c00cdbb3c3f12e
wlgp8563 <wlgp8563@naver.com>
2024-10-06 21:57:02 +0900

    InGame UI Update
    인게임 UI 업데이트
    선택지&체력바&레업바 등등

---
+++ N-own Magic/Assets/Scripts/BntsControl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BntsControl : MonoBehaviour
+{
+    public Button[] buttons;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons[0].onClick.AddListener(InGameBnt);
+        buttons[1].onClick.AddListener(InManual);
+        buttons[2].onClick.AddListener(GameExit);
+    }
+
+    public void InGameBnt()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void InManual()
+    {
+
+    }
+
+    public void GameExit()
+    {
+
+    }
+}
